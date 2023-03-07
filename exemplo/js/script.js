fa03b5263b9ad52db79cd31e4eea8487072d4ab5
MirellaRossi <rm97990@fiap.com.br>
2023-03-07 11:48:57 -0300

    script.js adicionado ao html

---
+++ exemplo/js/script.js
@@ -0,0 +1,15 @@
+//Este é um comentário de linha 
+
+/**
+ * Este é um comentário
+ * de
+ * bloco
+  
+ */
+
+
+//exemplo das funções do console. 
+console.log("Olá mundo")
+console.error("Bom dia")
+console.warn("Será?")
+console.info("Vai ser...")
